24cc823bf9708ac573b624f85fd9009a5ddbfa4b
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-01-04 16:35:07 +0800

    Create Vector.cpp

---
+++ Cpp/class/Vector/Vector.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+#include<cmath>
+#include"Vector.h"
+using std::atan;
+using std::atan2;
+using std::sqrt;
+using std::cos;
+using std::sin;
+using std::cout;
+
+namespace VECTOR{
+const double Rad_to_deg=45.0/atan(1.0);
+void Vector::set_mag(){
+mag=sqrt(x*x+y*y);
+}
+void Vector::set_ang(){
+if (x==0&y==0)
+ ang=0.0;
+else
+ ang=atan2(y,x);
+}
+void Vector::set_x(){
+  x=mag*cos(ang);
+}
+void Vector::set_y(){
+  y=mag*sin(ang);
+}
+void Vector::polar_mode(){
+mode=RECT;
+}
+void Vector::rect_mode(){
+mode=POL;
+}
+Vector::Vector(){
+x=y=ang=mag=0.0;
+mode=RECT;
+}
+Vector::Vector(double n1,double n2,Mode form){
+mode=form;
+if (mode==RECT)
+ { 
+  x=n1;
+  y=n2;
+  set_mag();
+  set_ang();
+ }
+ else if(mode==POL)
+ {
+  mag=n1;
+  ang=n2/Rad_to_deg;
+  set_x();
+  set_y();
+ }
+ else
+ {
+  cout<<"Incorrect 3rd argument to Vector()--";
+  cout<<"Vector set to 0\n";
+  x=y=mag=ang=0.0;
+  mode=RECT;
+  }
+} 
+void Vector::reset(double n1,double n2,Mode form){
+mode=form;
+if (mode==RECT)
+ { 
+  x=n1;
+  y=n2;
+  set_mag();
+  set_ang();
+ }
+ else if(mode==POL)
+ {
+  mag=n1;
+  ang=n2/Rad_to_deg;
+  set_x();
+  set_y();
+ }
+ else
+ {
+  cout<<"Incorrect 3rd argument to Vector()--";
+  cout<<"Vector set to 0\n";
+  x=y=mag=ang=0.0;
+  mode=RECT;
+  }
+} 
+Vector Vector::operator+(const Vector & b) const{
+   return Vector(x+b.x,y+b.y);
+}
+Vector Vector::operator-(const Vector & b) const{
+   return Vector(x-b.x,y-b.y);
+}
+Vector Vector::operator-() const{
+   return Vector(-x,-y);
+}
+Vector Vector::operator*(double n) const{
+   return Vector(x*n,y*n);
+}
+Vector operator*(double n,const Vector & a){
+   return a*n;
+}
+std::ostream& operator<<(std::ostream & os,const Vector & v){
+   if (v.mode==Vector::RECT)
+     os<<"(x,y)=("<<v.x<<","<<v.y<<")";
+   else if(v.mode==Vector:: POL)
+    {
+     os<<"(m,a)=("<<v.mag<<","<<v.ang*Rad_to_deg<<")";
+    }
+   else 
+     os<<"Vecotr object mode is invalid";
+   return os;
+}
+}
+
