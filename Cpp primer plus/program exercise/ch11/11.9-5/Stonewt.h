af9073577017cb402a3a29e412e76be9cab4d5ec
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-01-05 17:03:44 +0800

    Create Stonewt.h

---
+++ Cpp primer plus/program exercise/ch11/11.9-5/Stonewt.h
@@ -0,0 +1,25 @@
+#ifndef STONEWT_H_
+#define STONEWT_H_
+#include<iostream>
+
+class Stonewt{
+public:
+  enum Mode{stones,pounds_int,pounds_fp};
+private:
+  enum {Lbs_per_stn=14};
+  int stone;
+  double pds_left;
+  double pounds;
+  Mode mode;
+public:
+  Stonewt();
+  Stonewt(double lbs,Mode=stones);
+  Stonewt(int stn,double lbs,Mode=stones);
+  ~Stonewt(){};
+  Stonewt operator+(const Stonewt&s) const;
+  Stonewt operator-(const Stonewt&s) const;
+  Stonewt operator*(double n) const;
+  friend Stonewt operator*(double n,const Stonewt&s) ;
+  friend std::ostream& operator<<(std::ostream&os,const Stonewt&s);
+};
+#endif
