a40b38062674752490abbd1da2db60b74492129d
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-01-15 16:56:22 +0800

    Create class.h

---
+++ Cpp primer plus/program exercise/ch13/13.11/class.h
@@ -0,0 +1,28 @@
+
+class Cd{
+private:
+   char performers[50];
+   char label[20];
+   int selections;
+   double playtime;
+public:
+   Cd(const char* s1,const char* s2, int n, double x);
+   Cd(const Cd& d);
+   Cd();
+   virtual ~Cd(){}
+   virtual void Report() const;
+   Cd &operator=(const Cd& d);
+};
+
+class Classic:public Cd
+{
+private:
+char pwork[50];
+public:
+Classic();
+Classic(const char* s1,const char* s2,const char* s3,int n, double x);
+Classic(const Classic&d);
+virtual ~Classic(){}
+virtual void Report() const;
+Classic &operator=(const Classic& d);
+};
