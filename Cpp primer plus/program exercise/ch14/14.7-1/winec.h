d70835546d9e8be5a7bd2c43b734373ea2f19eff
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-01-21 19:01:45 +0800

    Create winec.h

---
+++ Cpp primer plus/program exercise/ch14/14.7-1/winec.h
@@ -0,0 +1,56 @@
+#ifndef WINE_H_
+#define WINE_H_
+
+#include<iostream>
+#include<string>
+#include<valarray>
+
+//Pairs: template class
+template<class T1,class T2>
+class Pair
+{
+private:
+  T1 a;
+  T2 b;
+public:
+  T1& first();
+  T2& second();
+  T1 first() const{return a;}
+  T2 second() const{return b;}
+  Pair(const T1& aval, const T2& bval):a(aval),b(bval){}
+  Pair(){}
+};
+
+template<class T1,class T2>
+T1& Pair<T1,T2>::first()
+{
+ return a;
+}
+
+template<class T1,class T2>
+T2& Pair<T1,T2>::second()
+{
+ return b;
+}
+
+//wine: class
+typedef std::valarray<int> ArrayInt;
+typedef Pair<ArrayInt,ArrayInt> PairArray;
+
+class Wine
+{
+private:
+  std::string name;
+  PairArray data;
+  int years;
+public:
+  Wine();
+  Wine(const char* l,int y,const int yt[],const int bot[]);
+  Wine(const char*l,int y);
+  void GetBottles();
+  const std::string& Label();
+  int sum();
+  void show();
+};
+
+#endif
