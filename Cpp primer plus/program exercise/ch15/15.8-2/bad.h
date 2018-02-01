0728973c8a3b302a3c7c22fc344c432cc4c9ba10
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-02-01 22:16:41 +0800

    Create bad.h

---
+++ Cpp primer plus/program exercise/ch15/15.8-2/bad.h
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdexcept>
+
+class bad_hmean:public std::logic_error
+{
+private:
+  double v1;
+  double v2;
+public:
+  bad_hmean(double a=0,double b=0,const char* str="invalid arguments:a=-b\n"):
+logic_error(str),v1(a),v2(b){}
+  
+};
+
+
+
+class bad_gmean:public std::logic_error
+{ 
+public:
+  double v1;
+  double v2;
+  bad_gmean(double a=0,double b=0,const char* str="gmean() arguments should be >= 0\n"):
+v1(a),v2(b),logic_error(str){}
+  const char*mesg();
+};
+
+
