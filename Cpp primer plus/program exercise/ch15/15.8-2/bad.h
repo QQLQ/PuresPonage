80050069cbec48822f6cbbbb0f9fe0f98982fed5
QQLQ <32114911+QQLQ@users.noreply.github.com>
2018-02-02 08:31:34 +0800

    Update bad.h

--- Cpp primer plus/program exercise/ch15/15.8-2/bad.h
+++ Cpp primer plus/program exercise/ch15/15.8-2/bad.h
@@ -9,7 +9,6 @@ private:
 public:
   bad_hmean(double a=0,double b=0,const char* str="invalid arguments:a=-b\n"):
 logic_error(str),v1(a),v2(b){}
-  
 };
 
 
@@ -21,7 +20,6 @@ public:
   double v2;
   bad_gmean(double a=0,double b=0,const char* str="gmean() arguments should be >= 0\n"):
 v1(a),v2(b),logic_error(str){}
-  const char*mesg();
 };
 
 
